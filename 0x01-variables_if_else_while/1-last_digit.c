2bf9bf2635567b860d9a25138136a044acc23bdd
Ahmed Abdel Nasser Fathy <ahmedfathyme.af@gmail.com>
2023-02-17 15:50:18 -0800

    Task.1 Commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,15 +20,15 @@ int main(void)
 	printf("Last digit of %d is %d", n, lastDigit);
 	if (lastDigit > 5)
 	{
-		printf("and is greater than 5");
+		printf(" and is greater than 5");
 	}
 	if (lastDigit == 0)
 	{
-		printf("and is 0");
+		printf(" and is 0");
 	}
 	if (lastDigit < 6 && lastDigit != 0)
 	{
-		printf("and is less than 6 and not 0");
+		printf(" and is less than 6 and not 0");
 	}
 
 	return (0);
