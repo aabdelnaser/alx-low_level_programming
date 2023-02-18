d56be11e9150f9750f86ed8e5bf1ad722836a25d
Ahmed Abdel Nasser Fathy <ahmedfathyme.af@gmail.com>
2023-02-17 16:03:41 -0800

    Task.2 Commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,9 +12,9 @@ int main(void)
 	char alphabetLetter;
 	for (alphabetLetter = 'a'; alphabetLetter <='z' ; alphabetLetter++)
 	{
-		putchar(alphabetLetter"\n");
+		putchar(alphabetLetter);
 
 	}
-	
+	putchar('\n');
 	return(0);
 }
