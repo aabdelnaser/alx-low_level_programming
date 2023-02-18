eace694c598cde2d6cbd64863e4dfd7269dd9ac3
Ahmed Abdel Nasser Fathy <ahmedfathyme.af@gmail.com>
2023-02-17 17:14:59 -0800

    Task.8 Commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints all the numbers of base 16 in lowercase, followed by a new line with putchar
+ *
+ *
+ * Return: Always 0 (Success)
+ * Name: Ahmed Abdel Nasser
+ */
+int main(void)
+{
+	int hexadecimalNumber;
+	char hexadecimalCharacter;
+
+	for (hexadecimalNumber = 48 ; hexadecimalNumber < 58 ; hexadecimalNumber++)
+	{
+		putchar(hexadecimalNumber);
+	}
+	for (hexadecimalCharacter = 'a' ; hexadecimalCharacter <= 'f' ; hexadecimalCharacter++)
+	{
+		putchar(hexadecimalCharacter);
+	}
+	putchar('\n');
+
+	return (0);
+}
