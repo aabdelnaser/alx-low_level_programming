0512c2d83804c5b0933e329ac811735589d4aee5
Ahmed Abdel Nasser Fathy <ahmedfathyme.af@gmail.com>
2023-02-17 16:44:23 -0800

    Task.5 Commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints all numbers of base 10
+ *
+ *
+ * Return: Always 0 (Success)
+ * Name: Ahmed Abdel Nasser
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0 ; number < 10 ; number++)
+	{
+		printf("%d", number);
+	}
+	printf("\n");
+
+	return (0);
