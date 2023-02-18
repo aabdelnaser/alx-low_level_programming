a2a1e1799554545f638095e32715a43edc2f8a33
Ahmed Abdel Nasser Fathy <ahmedfathyme.af@gmail.com>
2023-02-17 16:51:12 -0800

    Task.6 Commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints all numbers of base 10 with putchar
+ *
+ *
+ * Return: Always 0 (Success)
+ * Name: Ahmed Abdel Nasser
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0 ; number < 10 ; number++)
+	{
+		putchar(number);
+	}
+	putchar('\n');
+	
+	return (0);
+}
