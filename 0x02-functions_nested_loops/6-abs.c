7140a4400552b152f30b61178e65015d81db3e82
Ahmed Abdel Nasser Fathy <ahmedfathyme.af@gmail.com>
2023-02-22 07:19:14 -0800

    Task.6 Commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - Function that computes the absolute value of an integer.
+ * @i : An integer declared variable.
+ *
+ * Return: Void 
+ * Name: Ahmed Abdel Nasser
+ * ALX Software Engineering Cohort 12
+ */
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		i = i * -1;
+	}
+	return (i);
+}
