21a4db2c95a8235d392c59745a8c0cecd382087c
Ahmed Abdel Nasser Fathy <ahmedfathyme.af@gmail.com>
2023-02-22 05:10:21 -0800

    Task.1 Commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Program prints the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ * Name: Ahmed Abdel Nasser
+ * ALX Software Engineering Cohort 12
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for ( alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+	
+	_putchar('\n');
+
+	return (0);
+}
