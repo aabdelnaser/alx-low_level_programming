2a6544277009049b262fe9d9e816a46e495d1431
Ahmed Abdel Nasser Fathy <ahmedfathyme.af@gmail.com>
2023-02-22 08:02:54 -0800

    Task.7 Commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - Function that prints the last digit of a number.
+ * @lastDigit: An integer declared argument of the function.
+ *
+ * Return: The lastDigit variable.
+ * Name: Ahmed Abdel Nasser
+ * ALX Software Engineering Cohort 12
+ */
+int print_last_digit(int lastDigit)
+{
+	int tmpNumber;
+	
+	tmpNumber = lastDigit % 10;
+	if (tmpNumber < 0)
+	{
+		tmpNumber = tmpNumber * -1;
+	}
+	_putchar(tmpNumber);
+	return (tmpNumber);
+}
