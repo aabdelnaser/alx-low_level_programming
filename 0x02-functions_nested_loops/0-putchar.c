12f8b364ccc23d04b0ff93c8bc03599c0806cb7b
Ahmed Abdel Nasser Fathy <ahmedfathyme.af@gmail.com>
2023-02-22 05:00:12 -0800

    Task.0 Final Edit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -13,13 +13,13 @@ int main(void)
 {
 	char wordCharacters[] = "_putchar";
 	int i;
-	
+
 	for (i = 0 ; i < 8 ; i++)
 	{
 		_putchar(wordCharacters[i]);
 	}
-	
+
 	_putchar('\n');
-	
+
 	return (0);
 }
