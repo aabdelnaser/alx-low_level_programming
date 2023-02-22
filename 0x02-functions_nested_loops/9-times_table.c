2d4fc28ca6f563b9002b9f3c3e4a91334f64a2a7
Ahmed Abdel Nasser Fathy <ahmedfathyme.af@gmail.com>
2023-02-22 09:54:33 -0800

    Task.9 Commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -24,30 +24,4 @@ void times_table(void)
 		}
 		_putchar('\n');
 	}
-}i#include <stdio.h>
-#include "main.h"
-
-/**
- * times_table - Function that prints the 9 times table, starting with 0.
- *
- * Return: Void
- * Name: Ahmed Abdel Nasser
- * ALX Software Engineering Cohort 12
- */
-void times_table(void)
-{
-	int row, column, result;
-	
-	for (row = 0 ; row <= 8 ; row++)
-	{
-		for (column = 0 ; column <= 8 ; column++)
-		{
-			result = row * column;
-			_putchar((result / 10) + '0');
-			_putchar((result % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
-		}
-		_putchar('\n');
-	}
 }
