30c28c63a2c4f92663f5a46bcd3408e0916a2aec
Ahmed Abdel Nasser Fathy <ahmedfathyme.af@gmail.com>
2023-02-22 11:43:05 -0800

    Task.9 Commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,8 +21,6 @@ void times_table(void)
 			{
 				_putchar(result / 10);
 				_putchar((result % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
 			}
 			else
 			{
@@ -35,6 +33,7 @@ void times_table(void)
 				}
 			else
 				{
+					_putchar(',');
 					_putchar(' ');
 				}
 		}
