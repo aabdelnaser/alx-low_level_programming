b59f36a346a16f9f23f33d585ed3d62994523dd2
Ahmed Abdel Nasser Fathy <ahmedfathyme.af@gmail.com>
2023-02-22 13:11:08 -0800

    Task.11 Commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - Function that prints all natural numbers from n to 98,
+ *               followed by a new line.
+ * @n: Integer Declared Argument
+ *
+ * Return: Void
+ * Name: Ahmed Abdel Nasser
+ * ALX Software Engineering Cohort 12
+ */
+void print_to_98(int n)
+{
+	for (n = 0 ; n < 98 ; n++)
+	{
+		printf("d% ", n++);
+	}
+	return (n);
+}
