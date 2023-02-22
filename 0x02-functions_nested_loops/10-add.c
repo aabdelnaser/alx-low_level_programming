a78c9d951da59ea2d442d24d4748af33b437b5d3
Ahmed Abdel Nasser Fathy <ahmedfathyme.af@gmail.com>
2023-02-22 12:47:39 -0800

    Task.10 Commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - Function that adds two integers and returns the result.
+ * @num1: First Integer Declared Variable TO Be Added
+ * @num2: Second Integer Declared Variable To Be Added
+ *
+ * Returns: Always 0 (Success)
+ * Name: Ahmed Abdel Nasser
+ * ALX Software Engineering Cohort 12
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
