a8d8f5a287ca108465004bbc9cdc85901c978552
Ahmed Abdel Nasser Fathy <ahmedfathyme.af@gmail.com>
2023-02-24 07:53:17 -0800

    Task.0 Commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,15 +2,19 @@
 
 /**
 * main - tests function that prints if integer is positive or negative
-* Return: 0
+* @i: A declared Integer Variable to test Function
+*
+* Return: 0 (Success)
+* Name: Ahmed Abdel Nasser
+* ALX Software Engineering Cohort 12
 */
 
 int main(void)
 {
-        int i;
+	int i;
 
-        i = 98;
-        positive_or_negative(i);
+	i = 98;
+	positive_or_negative(i);
 
-        return (0);
+	return (0);
 }
