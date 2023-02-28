0bf38f5529dd13d8a7a82540b1be944e78cb50f3
Ahmed Abdel Nasser Fathy <ahmedfathyme.af@gmail.com>
2023-02-28 15:00:14 -0800

    Task.1 Commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - Function that swaps the values of two integers.
+ * @a: First Declared Integer Variable
+ * @b: Second Declared Integer Variable
+ * @tmp: Temporary Declared Variable
+ * Return: 0 Always (Success)
+ * Name: Ahmed Abdel Nasser Fathy
+ * ALX Software Engineering Cohort 12
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
