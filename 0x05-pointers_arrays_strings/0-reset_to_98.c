c026c11e2f1f2512a31f427a74b191ca6bd8c21a
Ahmed Abdel Nasser Fathy <ahmedfathyme.af@gmail.com>
2023-02-28 14:50:53 -0800

    Task.0 Commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes a pointer to an int as parameter
+ *               and updates the value it points to to 98
+ * @n: A declared integer variable
+ *
+ * Return: 0 Always (Success)
+ * Name: Ahmed Abdel Nasser
+ * ALX Software Engineering Cohort 12
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
