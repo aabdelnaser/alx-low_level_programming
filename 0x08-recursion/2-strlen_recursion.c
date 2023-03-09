0a7283488801aa9b934af5d99bbccd86dd908ba0
Ahmed Abdel Nasser Fathy <ahmedfathyme.af@gmail.com>
2023-03-08 16:26:34 -0800

    Task.2 Commit

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - A function that returns the length of a string.
+ * @s : A declared string variable
+ *
+ * Return : Always 0.
+ * Name: Ahmed Abdel Nasser Fathy
+ * ALX Software Engineering Cohort 12.
+ */
+int _strlen_recursion(char *s)
+{
+	int wordSize = 0;
+
+	if (*s)
+	{
+		wordSize++;
+		wordSize = wordSize + _strlen_recursion(s + 1);
+	}
+	return (wordSize);
+}
