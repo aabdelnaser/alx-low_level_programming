84362b4c13155bff4dfd9f2cca435eb5cf622e26
Ahmed Abdel Nasser Fathy <ahmedfathyme.af@gmail.com>
2023-03-08 16:44:33 -0800

    Task.3 Commit'

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * factorial - A function that returns
+ *             the factorial of a given number.
+ * n: A declared interger variable.
+ *
+ * Return:int
+ *
+ * Name: Ahmed Abdel Nasser Fathy
+ * ALX Software Engineering Cohort 12
+ */
+int factorial(int n)
+{
+	int res;
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n > 0)
+	{
+		res = n * factorial(n - 1);
+	}
+	else
+	{
+	return (1);
+	}
+}	
