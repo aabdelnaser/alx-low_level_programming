34ccc61055de3da341c4d021604dc1d13cce1bd2
Ahmed Abdel Nasser Fathy <ahmedfathyme.af@gmail.com>
2023-03-08 16:03:41 -0800

    Task.1 Commit

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - A function that prints a string in reverse.
+ * @s : A declared string variable.
+ *
+ * Return : Void
+ * Name : Ahmed Abdel Nasser Fathy
+ * ALX Software Engineering Cohort 12
+ */
+vvoid _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_print_rev_recursion(s - 1);
+		_putchar(*s)
+	}
+}
