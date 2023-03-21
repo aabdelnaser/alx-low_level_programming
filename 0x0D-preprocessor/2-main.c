84412c1f460e72d764ab195f254ef40b8f779bad
Ahmed Abdel Nasser Fathy <ahmedfathyme.af@gmail.com>
2023-03-20 23:53:00 -0700

    Task.2 Commit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -4,7 +4,6 @@
  *        , followed by a new line.
  *
  * Return: ALways 0
- * Description: Function printing the name of the file
  * Name : Ahmed Abdel Nasser
  * ALX Software Engineering Cohort 12
  */
