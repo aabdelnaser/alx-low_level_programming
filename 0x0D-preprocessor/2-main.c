fca24221c14b1c3556534cf6d9825f7c0741664a
Ahmed Abdel Nasser Fathy <ahmedfathyme.af@gmail.com>
2023-03-20 19:23:17 -0700

    Task.2 Commit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /*
  * main - function that prints the name of the file it
  *        was compiled from, followed by a new line.
@@ -9,6 +8,5 @@
 int main(void)
 {
 	printf("%s\n", __FILE__);
-	
 	return (0);
 }
