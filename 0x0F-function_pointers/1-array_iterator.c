37364b8847f24f45b492f2de85a61ad9bf50c230
Ahmed Abdel Nasser Fathy <ahmedfathyme.af@gmail.com>
2023-03-22 18:01:16 -0700

    Task.1 Commit

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -18,9 +18,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array == NULL  && action = NULL)
 	{
-		for (i = 0 ; i  < (int)  size ; i++)
-		{
-			action(array[i]);
-		}
+		return;
+	}
+	for (i = 0 ; i  < (int)  size ; i++)
+	{
+		action(array[i]);
 	}
 }
