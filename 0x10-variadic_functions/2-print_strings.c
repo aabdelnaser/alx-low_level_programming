812e40d36007e96db1a3630a5af8c16af5e71baf
Ahmed Abdel Nasser Fathy <ahmedfathyme.af@gmail.com>
2023-03-23 19:43:13 -0700

    Task.2 Commit

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,10 +1,11 @@
 #include "variadic_functions.h"
 /**
- * print_strings - unction that prints strings, followed by a new line.
+ * print_strings - function that prints strings
+ *                , followed by a new line.
  * @separator: String Separation
  * @n: number of parameters
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  * Name: Ahmed Abdel Nasser
  * ALX Software Engineering Cohort 12
  */
@@ -22,11 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	while (i < n)
 	{
 		str = va_arg(alist, char*);
-		if (str == NULL)
-		{
-			str = "(nil)";
-		}
-		printf("%s", str);
+		printf("%s", (str  == NULL) ? "(nil)" : str);
 		if (i < n - 1)
 		{
 			printf("%s", separator);
