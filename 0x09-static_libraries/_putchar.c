a3b5365573b754a5d8d70f943f951f9a84508047
Ahmed Abdel Nasser Fathy <ahmedfathyme.af@gmail.com>
2023-03-24 06:51:14 -0700

    _Putchar Commit

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,18 +1,13 @@
 #include <unistd.h>
-
-
-
 /**
+ * _putchar - writes the character
  *
- *  * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- *   * @c: The character to print
- *
- *    *
- *
- *     * Return: On success 1.
- *
- *      * On error, -1 is returned, and errno is set appropriately*/
+ * Return: On success 1.
+ * Name : Ahmed Abdel Nasser
+ * ALX Software Engineering Cohort 12
+ */
 
 int _putchar(char c)
 
