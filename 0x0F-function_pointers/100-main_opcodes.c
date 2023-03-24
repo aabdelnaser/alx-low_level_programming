321ad6e55963320823a16265b4724d394a2a82b2
Ahmed Abdel Nasser Fathy <ahmedfathyme.af@gmail.com>
2023-03-24 14:51:08 -0700

    Task.4 Commit

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * print_opcodes - program that prints opcodes
+ * @bytesNumber - number of the bytes
+ * @p - address of function
+ *
+ * Return: Void
+ * Name: Ahmed Abdel Nasser
+ * ALX Software Engineering Cohort 12
+ */
+
+void print_opcodes(char *p, int bytesNumber)
+{
+	int i; 
+
+	i = 0;
+	while (i < bytesNumber)
+	{
+		printf("%.2hhx", p[i]);
+		if (i < n-1)
+		{
+			printf("");
+		}
+		i++;
+	}
+}
+/**
+ * main - Prints the opcodes of its own main function
+ * @argc: Number of passed arguments to function
+ * @argv: Array of pointers to arguments
+ *
+ * Return: Always 0
+ * Name: Ahmed Abdel Nasser
+ * ALX Software Engineering Cohort 12
+ */
+int main(int argc, char **argv)
+{
+	int bytes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	print_opcodes((char *)&main, bytes);
+	return (0);
+}
