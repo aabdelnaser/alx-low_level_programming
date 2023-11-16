041942dd309793d14dbac8f9ff8af238d5df78cc
Ahmed Abdel Nasser <ahmedfathyme@yahoo.com>
2023-11-16 12:14:31 -0800

    Task.0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion - A function that prints a string, followed by a new line.
+ * @s : The Printed String
+ *
+ * Return - Always 0
+ *
+ * Name: Ahmed Abdel Nasser
+ * Email: ahmedfathyme@yahoo.com
+ */
+void _puts_recursion(char *s)
+{
+	if(*s == '/0')
+	{
+		return _putchar('/n');
+	}
+	return _putchar(*s);
+}
