0bdcf830267fde52af6436f5988ef2910e64156c
Ahmed Abdel Nasser <ahmedfathyme@yahoo.com>
2023-11-16 12:24:10 -0800

    Task.0 Commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -12,9 +12,10 @@ void _puts_recursion(char *s)
 {
 	if(*s == '\0')
 	{
-		return _putchar('\n');
+		_putchar('\n');
+		return 0;
 	}
-	return _putchar(*s);
+	_putchar(*s);
 	_puts_recursion(s+1);
 }
 
