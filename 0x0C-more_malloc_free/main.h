76f2f2d6858d78610599db469bbb128c3b279771
Ahmed Abdel Nasser <ahmedfathyme@yahoo.com>
2023-11-24 14:35:58 -0800

    Task.0 Commit

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
 
 void *malloc_checked(unsigned int b);
 
