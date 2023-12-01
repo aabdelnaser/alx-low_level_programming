e84c4dafb12542593cef0271ac2bda28fda1b606
Ahmed Abdel Nasser Fathy <ahmedfathyme.af@gmail.com>
2023-11-30 18:36:07 -0800

    Task.2 Commit

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -25,7 +25,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		string = va_arg(newString, char *);
 
 
-		if (str == NULL)
+		if (string == NULL)
 			printf("(nil)");
 		else
 			printf("%s", str);
