af1cfc79093a1a9490d2017c3834ae89f40001a8
Ahmed Abdel Nasser <ahmedfathyme.af@gmail.com>
2023-12-24 03:46:47 -0800

    New 0.putchar.c file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - It's a function that prints _putchar followed by a new line.
+ *  
+ * Return : Always 0 (Success)
+ * Name: Ahmed Abdel Nasser
+ * Email: ahmedfathyme.af@gmail.com
+ */
+int main (void)
+{
+	putchar_letters [] = { _, p, u, t, c, h, a, r};
+	for (int i = 0; i < 8; i++)
+	{
+		_putchar(putchar_letters[i]);
+	}
+	_putchar('\n');
+
+	return(0);
+}
