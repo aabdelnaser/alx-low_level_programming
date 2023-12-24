700d877fb9d9352cd5757e19f4c820eb2f124c47
Ahmed Abdel Nasser <ahmedfathyme.af@gmail.com>
2023-12-24 12:13:21 -0800

    Task.1 Commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - a function that prints an alphabet in lowercase,
+ *                  followed by new line.
+ *
+ * Return: Void
+ * Name: Ahmed Abdel Nasser
+ * E-mail: ahmedfathyme.af@gmail.com
+ */
+void print_alphabet(void)
+{
+	char lowerCaseAlphabet;
+
+	for(lowerCaseAlphabet = 97; lowerCaseAlphabet <= 122; lowerCaseAlphabet++)
+	{
+		_putchar(lowerCaseAlphabet);
+	}
+	_putchar('\n');
+}
