1246d251016a7ea0857bd0eedb13587b2786b0b1
Ahmed Abdel Nasser <ahmedfathyme.af@gmail.com>
2023-12-24 04:00:41 -0800

    Betty Doc Issues Resolved

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - It's a function that prints _putchar followed by a new line.
- *  
+ * main - It's a program that prints _putchar followed by a new line and return 0.
+ * 
  * Return : Always 0 (Success)
  * Name: Ahmed Abdel Nasser
  * Email: ahmedfathyme.af@gmail.com
  */
-int main (void)
+int main(void)
 {
 	int i;
 	char putchar_letters[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+
 	for (i = 0; i < 8; i++)
 	{
 		_putchar(putchar_letters[i]);
 	}
 	_putchar('\n');
 
-	return(0);
+	return (0);
 }
