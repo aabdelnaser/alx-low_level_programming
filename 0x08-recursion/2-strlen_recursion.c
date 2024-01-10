272d1b8e5e22849a6a1614f0d07709bb32d9ed63
Ahmed Abdel Nasser <ahmedfathyme.af@gmail.com>
2024-01-10 13:34:41 -0800

    Task.2 Commit

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,13 +10,8 @@
  */
 int _strlen_recursion(char *s)
 {
-	int count;
-
-	count = 0;
 	if (*s != '\0')
 	{
-		count++;
-		_strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 	}
-	return (count);
 }
