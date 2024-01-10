f8b940aa9266c0a39653ba5d41e42ef05dccdf48
Ahmed Abdel Nasser <ahmedfathyme.af@gmail.com>
2024-01-10 14:24:40 -0800

    Task.4 Commit

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,7 +2,7 @@
 /**
  * _pow_recursion - a function that returns the value of x,
  *                  raised to the power of y
- * 
+ *
  * @x : The base Number
  * @y : The power number
  *
@@ -14,8 +14,12 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
+	else if (y == 0)
+	{
+		return (1);
+	}
 	else
 	{
-		return (x * _pow_recursion(x , y - 1));
+		return (x * _pow_recursion(x, y - 1));
 	}
 }
