de19150856f4b172f9754c39583d71a76de289bb
Ahmed Abdel Nasser <ahmedfathyme.af@gmail.com>
2024-01-10 13:18:50 -0800

    Task.2 Commit:

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strlen_recursion - a function that returns the length of a string.
+ * @s : The string that we calculate its length.
+ *
+ * Return : 0 Always Success.
+ *
+ * Ahmed Abdel Nasser
+ * ALX Software Engineering Cohort 22
+ */
+int _strlen_recursion(char *s)
+{
+	count = 0;
+
+	if (*s != '\0')
+	{
+		count++
+		_strlen_recursion(s + 1);
+	}
+	return 0;
+}
