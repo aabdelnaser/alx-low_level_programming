4cdb745a889ad50bc6ee77641193bc490d76850a
Ahmed Abdel Nasser <ahmedfathyme.af@gmail.com>
2024-01-11 12:30:33 -0800

    Betty Code Styling Resolved

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * _sqrt_recursion_helper - a helper function that returns the natural square root of a number.
+ * _sqrt_recursion_helper - a helper function that returns
+ *                          the natural square root of a number.
  * @n : The number that we should find its square root.
  * @rootNumber: The root that will be multiplied by itself
  *
  * Return: -1 in Failure, Otherwise will return the root number.
  */
- int _sqrt_recursion_helper(int n, int rootNumber)
+int _sqrt_recursion_helper(int n, int rootNumber)
 {
 	if ((rootNumber * rootNumber) > n)
 	{
@@ -22,12 +23,13 @@
 	}
 }
 /**
- * _sqrt_recursion - a function that returns the natural square root of a number.
+ * _sqrt_recursion - a function that returns
+ *                   the natural square root of a number.
  * @n : The number that we should find its square root.
  *
  * Return: -1 in Failure, Otherwise will call the helper function
  */
- int _sqrt_recursion(int n)
+int _sqrt_recursion(int n)
 {
 	if (n < 0)
 	{
